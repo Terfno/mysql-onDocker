df5621c9bc77d46c59a97922df91e16950946c7c
terfno <sueda.takahito@gmail.com>
2020-06-22 11:26:25 +0900

    :shower:

--- db/sql/7-select.sql
+++
@@ -1,2 +0,0 @@
-select 名前 from ken2015 where 名前!=庁舎 and 地方='中国';
-select 名前, 人口 from ken2015 where 人口<600000;
--- db/sql/8-createTable.sql
+++
@@ -1,8 +0,0 @@
-create table peakDesignBags(
-  id INT NOT NULL AUTO_INCREMENT,
-  version INT NOT NULL,
-  line VARCHAR(30) NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  capacity INT,
-  PRIMARY KEY (id)
-);
